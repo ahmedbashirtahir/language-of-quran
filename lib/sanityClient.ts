8dfa0fc590c38f2119f8c5472ca6c1b9ef566fc8
ahmedbashirtahir <ahmedbashirtahir@users.noreply.github.com>
2025-07-10 13:51:24 +0500

    Update sanityClient.ts

--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -4,8 +4,10 @@ import { createClient } from '@sanity/client';
 export const sanityClient = createClient({
   // Find your project ID and dataset in your sanity.json or at manage.sanity.io
   // You will need to replace these with your actual project details
-  projectId: 'YOUR_PROJECT_ID',
+  projectId: 'pdfk1jvf',
   dataset: 'production',
   useCdn: true, // `false` if you want to ensure fresh data
   apiVersion: '2024-05-01', // use a UTC date in YYYY-MM-DD format
 });
+
+
